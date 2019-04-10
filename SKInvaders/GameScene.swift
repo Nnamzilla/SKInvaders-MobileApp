03600023c05b87b72f6f6f15fcce8dd824a70a22
Nnamdi Massally <nqm14a@dmc-207-11.local>
2019-04-10 08:19:41 -0500

    Initial Commit

---
+++ SKInvaders/GameScene.swift
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import SpriteKit
+import CoreMotion
+
+class GameScene: SKScene {
+  
+  // Private GameScene Properties
+  
+  var contentCreated = false
+    
+    enum InvaderType {
+        case a
+        case b
+        case c
+        
+        static var size: CGSize {
+            return CGSize(width: 24, height: 16)
+        }
+        
+        static var name: String {
+            return "invader"
+        }
+    }
+    
+    let kInvaderGridSpacing = CGSize(width: 12, height: 12)
+    let kInvaderRowCount = 6
+    let kInvaderColCount = 6
+  
+  // Object Lifecycle Management
+  
+  // Scene Setup and Content Creation
+  override func didMove(to view: SKView) {
+    
+    if (!self.contentCreated) {
+      self.createContent()
+      self.contentCreated = true
+    }
+  }
+  
+  func createContent() {
+    
+//    let invader = SKSpriteNode(imageNamed: "InvaderA_00.png")
+//
+//    invader.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+//
+//    self.addChild(invader)
+    
+    setupInvaders()
+    
+    // black space color
+    self.backgroundColor = SKColor.black
+  }
+    
+    func makeInvader(ofType invaderType: InvaderType) -> SKNode {
+        // 1
+        var invaderColor: SKColor
+        
+        switch(invaderType) {
+        case .a:
+            invaderColor = SKColor.red
+        case .b:
+            invaderColor = SKColor.green
+        case .c:
+            invaderColor = SKColor.blue
+        }
+        
+        // 2
+        let invader = SKSpriteNode(color: invaderColor, size: InvaderType.size)
+        invader.name = InvaderType.name
+        
+        return invader
+    }
+    
+    func setupInvaders() {
+        // 1
+        let baseOrigin = CGPoint(x: size.width / 3, y: size.height / 2)
+        
+        for row in 0..<kInvaderRowCount {
+            // 2
+            var invaderType: InvaderType
+            
+            if row % 3 == 0 {
+                invaderType = .a
+            } else if row % 3 == 1 {
+                invaderType = .b
+            } else {
+                invaderType = .c
+            }
+            
+            // 3
+            let invaderPositionY = CGFloat(row) * (InvaderType.size.height * 2) + baseOrigin.y
+            
+            var invaderPosition = CGPoint(x: baseOrigin.x, y: invaderPositionY)
+            
+            // 4
+            for _ in 1..<kInvaderColCount {
+                // 5
+                let invader = makeInvader(ofType: invaderType)
+                invader.position = invaderPosition
+                
+                addChild(invader)
+                
+                invaderPosition = CGPoint(
+                    x: invaderPosition.x + InvaderType.size.width + kInvaderGridSpacing.width,
+                    y: invaderPositionY
+                )
+            }
+        }
+    }
+  
+  // Scene Update
+  
+  override func update(_ currentTime: TimeInterval) {
+    /* Called before each frame is rendered */
+  }
+  
+  // Scene Update Helpers
+  
+  // Invader Movement Helpers
+  
+  // Bullet Helpers
+  
+  // User Tap Helpers
+  
+  // HUD Helpers
+  
+  // Physics Contact Helpers
+  
+  // Game End Helpers
+  
+}
