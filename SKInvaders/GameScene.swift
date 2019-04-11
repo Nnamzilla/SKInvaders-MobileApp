7e6016f2f123541f23caca437b5dc4c6d74750d5
Nnamdi Massally <nqm14a@lib-301-10-9117.local>
2019-04-11 11:47:37 -0500

    Supposedly the ship is supposed to tilt, but I don't know how to get it to tilt on the... emulator?

--- SKInvaders/GameScene.swift
+++ SKInvaders/GameScene.swift
@@ -26,9 +26,19 @@ import CoreMotion
 class GameScene: SKScene {
   
   // Private GameScene Properties
+    
+    let motionManager = CMMotionManager()
   
   var contentCreated = false
     
+    enum InvaderMovementDirection {
+        case right
+        case left
+        case downThenRight
+        case downThenLeft
+        case none
+    }
+
     enum InvaderType {
         case a
         case b
@@ -46,6 +56,12 @@ class GameScene: SKScene {
     let kInvaderGridSpacing = CGSize(width: 12, height: 12)
     let kInvaderRowCount = 6
     let kInvaderColCount = 6
+    let kShipSize = CGSize(width: 30, height: 16)
+    let kShipName = "ship"
+    let kScoreHudName = "scoreHud"
+    let kHealthHudName = "healthHud"
+
+
   
   // Object Lifecycle Management
   
@@ -55,6 +71,8 @@ class GameScene: SKScene {
     if (!self.contentCreated) {
       self.createContent()
       self.contentCreated = true
+      motionManager.startAccelerometerUpdates()
+
     }
   }
   
@@ -66,11 +84,25 @@ class GameScene: SKScene {
 //
 //    self.addChild(invader)
     
+    physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    
     setupInvaders()
+    setupShip()
+    setupHud()
     
     // black space color
     self.backgroundColor = SKColor.black
   }
+    
+    // 1
+    var invaderMovementDirection: InvaderMovementDirection = .right
+    // 2
+    var timeOfLastMove: CFTimeInterval = 0.0
+    // 3
+    let timePerMove: CFTimeInterval = 1.0
+    
+    
+
     
     func makeInvader(ofType invaderType: InvaderType) -> SKNode {
         // 1
@@ -128,17 +160,171 @@ class GameScene: SKScene {
             }
         }
     }
+    
+    func setupShip() {
+        // 1
+        let ship = makeShip()
+        
+        // 2
+        ship.position = CGPoint(x: size.width / 2.0, y: kShipSize.height / 2.0)
+        addChild(ship)
+    }
+    
+    func makeShip() -> SKNode {
+        let ship = SKSpriteNode(color: SKColor.green, size: kShipSize)
+        ship.name = kShipName
+        
+        // 1
+        ship.physicsBody = SKPhysicsBody(rectangleOf: ship.frame.size)
+        
+        // 2
+        ship.physicsBody!.isDynamic = true
+        
+        // 3
+        ship.physicsBody!.affectedByGravity = false
+        
+        // 4
+        ship.physicsBody!.mass = 0.02
+
+        return ship
+    }
+    
+    func setupHud() {
+        // 1
+        let scoreLabel = SKLabelNode(fontNamed: "Courier")
+        scoreLabel.name = kScoreHudName
+        scoreLabel.fontSize = 25
+        
+        // 2
+        scoreLabel.fontColor = SKColor.green
+        scoreLabel.text = String(format: "Score: %04u", 0)
+        
+        // 3
+        scoreLabel.position = CGPoint(
+            x: frame.size.width / 2,
+            y: size.height - (40 + scoreLabel.frame.size.height/2)
+        )
+        addChild(scoreLabel)
+        
+        // 4
+        let healthLabel = SKLabelNode(fontNamed: "Courier")
+        healthLabel.name = kHealthHudName
+        healthLabel.fontSize = 25
+        
+        // 5
+        healthLabel.fontColor = SKColor.red
+        healthLabel.text = String(format: "Health: %.1f%%", 100.0)
+        
+        // 6
+        healthLabel.position = CGPoint(
+            x: frame.size.width / 2,
+            y: size.height - (80 + healthLabel.frame.size.height/2)
+        )
+        addChild(healthLabel)
+    }
+
+
   
   // Scene Update
+    
+    func moveInvaders(forUpdate currentTime: CFTimeInterval) {
+        // 1
+        if (currentTime - timeOfLastMove < timePerMove) {
+            return
+        }
+        determineInvaderMovementDirection()
+        
+        // 2
+        enumerateChildNodes(withName: InvaderType.name) { node, stop in
+            switch self.invaderMovementDirection {
+            case .right:
+                node.position = CGPoint(x: node.position.x + 10, y: node.position.y)
+            case .left:
+                node.position = CGPoint(x: node.position.x - 10, y: node.position.y)
+            case .downThenLeft, .downThenRight:
+                node.position = CGPoint(x: node.position.x, y: node.position.y - 10)
+            case .none:
+                break
+            }
+            
+            // 3
+            self.timeOfLastMove = currentTime
+            
+            // 4
+            
+        }
+    }
+    
+    func processUserMotion(forUpdate currentTime: CFTimeInterval) {
+        // 1
+        if let ship = childNode(withName: kShipName) as? SKSpriteNode {
+            // 2
+            if let data = motionManager.accelerometerData {
+                // 3
+                if fabs(data.acceleration.x) > 0.2 {
+                    // 4 How do you move the ship?
+                    ship.physicsBody!.applyForce(CGVector(dx: 40 * CGFloat(data.acceleration.x), dy: 0))
+                }
+            }
+        }
+    }
+
+
   
   override func update(_ currentTime: TimeInterval) {
     /* Called before each frame is rendered */
+    processUserMotion(forUpdate: currentTime)
+    moveInvaders(forUpdate: currentTime)
   }
   
   // Scene Update Helpers
   
   // Invader Movement Helpers
-  
+    func determineInvaderMovementDirection() {
+        // 1
+        var proposedMovementDirection: InvaderMovementDirection = invaderMovementDirection
+        
+        // 2
+        enumerateChildNodes(withName: InvaderType.name) { node, stop in
+            
+            switch self.invaderMovementDirection {
+            case .right:
+                //3
+                if (node.frame.maxX >= node.scene!.size.width - 1.0) {
+                    proposedMovementDirection = .downThenLeft
+                    
+                    stop.pointee = true
+                }
+            case .left:
+                //4
+                if (node.frame.minX <= 1.0) {
+                    proposedMovementDirection = .downThenRight
+                    
+                    stop.pointee = true
+                }
+                
+            case .downThenLeft:
+                proposedMovementDirection = .left
+                
+                stop.pointee = true
+                
+            case .downThenRight:
+                proposedMovementDirection = .right
+                
+                stop.pointee = true
+                
+            default:
+                break
+            }
+            
+        }
+        
+        //7
+        if (proposedMovementDirection != invaderMovementDirection) {
+            invaderMovementDirection = proposedMovementDirection
+        }
+    }
+
   // Bullet Helpers
   
   // User Tap Helpers
